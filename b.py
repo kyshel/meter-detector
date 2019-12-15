9708a61b5dcc40996f94fc28c606236fd4325165
unknown <sunshuai@qianxin-inc.cn>
2019-12-16 02:28:15 +0800

    2

--- b.py
+++ b.py
@@ -1,30 +1,71 @@
 import cv2
 import numpy as np
+from pprint import pprint
+import logging
 
-img = cv2.imread('1.jpg')
-
-
-
-
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.04)
-
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst,None)
-
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0,0,255]
+logging.basicConfig(
+	level=logging.INFO,
+	# format='%(asctime)s %(name)-1s %(levelname)-3s %(message)s',
+	#format='%(levelname)-3s %(message)s',
+	format='%(levelname)-3s [%(filename)s:%(lineno)d] %(message)s',
+	datefmt='%y-%m-%d %H:%M:%S',
+	handlers=[
+	# logging.FileHandler(FILENAME_LOGGING),
+	logging.StreamHandler()
+	]
+)
 
 
-
+ 
 
 
 
 
+img_origin = cv2.imread('2.jpg')
+
+
+
+img = img_origin
+height, width, channels = img.shape
+
+
+
+for i in range(0,height-1):
+	for j in range(0,width-1):
+ 
+		cur_point = img[i][j]
+
+		logging.info(cur_point)
+
+ 
+
+
+
+		shift = 10
+		r=235
+		g=2
+		b=0
+
+		if  (r-shift <= cur_point[2] <= r+shift) and \
+			(g-shift <= cur_point[1] <= g+shift) and \
+			(b-shift <= cur_point[0] <= b+shift) :
+				logging.info('1')
+				logging.info(cur_point)
+
+
+
+	# 	break
+	# break
+
+
+
+
+
+
+
+# pprint(img)
 
 
 cv2.imshow('img',img)
-cv2.waitKey(0)
+cv2.waitKey(0) 
 cv2.destroyAllWindows()
